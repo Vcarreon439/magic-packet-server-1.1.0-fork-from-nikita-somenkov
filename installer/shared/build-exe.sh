97e4f0f7c5ef06997dea913c85c0df6bb4748da0
Víctor Hugo Carreón Pulido <hv_carreon@hotmail.com>
2026-01-10 14:19:06 -0600

    Fix build script path resolution

--- installer/shared/build-exe.sh
+++ installer/shared/build-exe.sh
@@ -5,19 +5,20 @@ set -x
 
 platform=$1
 binpath=$2
-topdir="$(cd "$(dirname "$(which "$0")")/../../"; pwd -P)"
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)"
+topdir="$(cd "${script_dir}/../../"; pwd -P)"
 pyinstaller_bin=pyinstaller
 
-cd $topdir
+cd "$topdir"
 
 # Used for override entry point for pyinstaller-many-linux, useful for CI build
 test -n "$CONFIG_TOPDIR" && topdir="$CONFIG_TOPDIR"
 test -n "$CONFIG_PYINSTALLER_BIN" && pyinstaller_bin="$CONFIG_PYINSTALLER_BIN"
 
-distpath=$topdir/dist/$platform/$binpath
-workpath=$topdir/build/$platform
-specpath=$topdir/build/$platform
-iconpath=$topdir/installer/resources/magic-packet-icon.ico
+distpath="$topdir/dist/$platform/$binpath"
+workpath="$topdir/build/$platform"
+specpath="$topdir/build/$platform"
+iconpath="$topdir/installer/resources/magic-packet-icon.ico"
 
 common_options=(
     --onefile
